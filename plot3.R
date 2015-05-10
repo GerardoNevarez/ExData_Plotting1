ef69d33b9a55f5049625456f1436f6efbf2e3bdf
GerardoNevarez <gnevarez@gmail.com>
2015-05-10 13:26:03 -0400

    Reformat plotting commands

--- plot3.R
+++ plot3.R
@@ -52,11 +52,11 @@ if (file.exists("power.txt")){
 #########
 png("plot3.png",width=480,height=480)
 
-plot( power$Sub_metering_1 ~ power$Time,type="n",ylab="Energy sub metering", xlab="")
-lines(power$Sub_metering_1 ~ power$Time , col="black")
-lines(power$Sub_metering_2 ~ power$Time , col="red")
-lines(power$Sub_metering_3 ~ power$Time , col="blue")
-     
+with(power, plot( Sub_metering_1 ~ Time, type="n", ylab="Energy sub metering", xlab=""))
+with(power, lines(Sub_metering_1 ~ Time, col="black"))
+with(power, lines(Sub_metering_2 ~ Time, col="red"))
+with(power, lines(Sub_metering_3 ~ Time, col="blue"))
+
 legend("topright",
   c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
   col=c("black","red","blue"),

7d45479e65a002ee66cf98b7b74226689edd3958
GerardoNevarez <gnevarez@gmail.com>
2015-05-10 14:34:37 -0400

    Read data from the network (HTTPS url) and pre-process - #TODO move to common file

--- plot4.R
+++ plot4.R
@@ -9,43 +9,56 @@
 
 #Auxiliary class conversion function to transform the Date column
 setAs("character","myDate", function(from){
-    d <- as.Date(from, format="%d/%m/%Y")
-    
-    #result stored on global scopt, to be used as date for time conversion
-    x <<- d
-    
-    d
-    })
+  d <- as.Date(from, format="%d/%m/%Y")
+  
+  #result stored on global scopt, to be used as date for time conversion
+  x <<- d
+  
+  d
+})
 
 #Auxiliary class conversion function to transform the Time column
 setAs("character","myTime", function(from) {
-    z <- strptime(from, format="%H:%M:%S")
-    t <-strptime(paste(as.character(x),
-                   formatC(z$hour, width=2, flag="0"),":",
-                   formatC(z$min, width=2, flag="0"),":",
-                   formatC(z$sec, width=2, flag="0")),
-             format="%Y-%m-%d %H : %M : %S")
-    as.POSIXct(t)
-    })
+  z <- strptime(from, format="%H:%M:%S")
+  t <-strptime(paste(as.character(x),
+                     formatC(z$hour, width=2, flag="0"),":",
+                     formatC(z$min, width=2, flag="0"),":",
+                     formatC(z$sec, width=2, flag="0")),
+               format="%Y-%m-%d %H : %M : %S")
+  as.POSIXct(t)
+})
 
 #avoid warnings 
 setClass('myDate')
 setClass('myTime')
 
-# Review if a subset of the data file exists, and read the data.
-# Create the secondary file using Unix command line tools:
+
+# Get data from the network, unzip, and pre-process before loading it in R 
+
+# if using MS Windows, the shell script might fail...
+# Use a Git Bash or Cygwin console to create the secondary data file
+# using Unix command line tools:
 #
 # head -n 1 household_power_consumption.txt > power.txt
 # grep "^[1|2]/2/2007" household_power_consumption.txt >> power.txt
 #
-# Windows Users -> use a Git Bash console 
 
-if (file.exists("power.txt")){
-    power <- read.table("power.txt",sep=";", na.strings="?", header=TRUE,
-        colClasses = c('myDate','myTime','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
-} else {
-    stop("Please subset the data file executing \"sh subset_data_file.sh\" using Unix/Git Bash  console")
+if (!file.exists("power.txt")){
+  if(!file.exists("household_power_consumption.txt")){
+    if(!file.exists("household_power_consumption.zip")){
+      download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                    "household_power_consumption.zip",
+                    method="curl")
+      # "internal" dows not support https
+    }
+    unzip("household_power_consumption.zip")
+  }
+  system("/bin/sh subset_data_file.sh")
 }
+
+power <- read.table("power.txt",sep=";", na.strings="?", header=TRUE,
+                    colClasses = c('myDate','myTime','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
 
 #########
 # Plot 4
@@ -75,11 +88,11 @@ with(power, lines(Sub_metering_2 ~ Time, col="red"))
 with(power, lines(Sub_metering_3 ~ Time, col="blue"))
 
 legend("topright",
-  c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-  col=c("black","red","blue"),
-  lwd=1,
-  horiz=FALSE,
-  bty="n")
+       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lwd=1,
+       horiz=FALSE,
+       bty="n")
 
 # graph (1,2)
 with(power,
